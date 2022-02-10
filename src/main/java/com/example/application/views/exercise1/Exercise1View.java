1a56b912615ec107bd735a429340e125d7c68bd1
nonWhistle <99025186+nonWhistle@users.noreply.github.com>
2022-02-10 18:35:24 +0000

    Exercise 3 completed.

--- src/main/java/com/example/application/views/exercise1/Exercise1View.java
+++ src/main/java/com/example/application/views/exercise1/Exercise1View.java
@@ -1,33 +1,78 @@
 package com.example.application.views.exercise1;
 
 import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.router.*;
 
 @PageTitle("Exercise 1")
 @Route(value = "Exercise-1", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 public class Exercise1View extends VerticalLayout {
 
-    public Exercise1View() {
-        setSpacing(false);
+    private final HorizontalLayout layout;
+    private Div navigation;
+    private Div content;
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+    public Exercise1View() 
+    {
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        setPadding(false);
+        setSpacing(false);
+        setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
+        Image img = new Image("https://upload.wikimedia.org/wikipedia/en/d/d9/Logoplaste_logo.svg", "Error");
+        img.setWidth("600px");
+
+        final Div header = new Div();
+        header.addClassNames("header");
+        header.setText("This is the header, 150 pixels top - bottom");
+        header.setHeight("150px");
+        header.getStyle().set("flexShrink", "0");
+        header.getStyle().set("background", "blue");
+        header.add(img);
+
+        layout = new HorizontalLayout();
+        layout.setHeightFull();
+        layout.setSpacing(false);
+        createTextLayout();
+
+        final Div footer = new Div();
+        footer.addClassNames("footer");
+        footer.setText("This is the footer, 100 pixels top - bottom");
+        footer.setHeight("100px");
+        footer.getStyle().set("flexShrink", "0");
+        footer.getStyle().set("background", "red");
+
+        add(header, layout, footer);
+
+        expand(layout);
+
     }
 
+    private void createTextLayout()
+    {
+        navigation = new Div();
+        navigation.addClassNames("navigation");
+        navigation.setWidth("25%");
+        navigation.getElement().getStyle().set("flex-shrink", "0");
+        navigation.setText("This is the navigation menu, My width is 25% of the app layout");
+        navigation.getStyle().set("background", "green");
+        DatePicker date = new DatePicker();
+        navigation.add(date);
+
+        content = new Div();
+        content.addClassNames("content");
+        content.setHeightFull();
+        content.getStyle().set("display", "flex");
+        content.setText("This is the content menu");
+        content.getStyle().set("alignContent", "start");
+        content.getStyle().set("background", "yellow");
+
+        layout.add(navigation, content);
+        layout.expand(content);
+        layout.setDefaultVerticalComponentAlignment(Alignment.STRETCH);
+    }
 }
--- src/main/java/com/example/application/views/exercise2/Exercise2View.java
+++ src/main/java/com/example/application/views/exercise2/Exercise2View.java
@@ -1,10 +1,19 @@
 package com.example.application.views.exercise2;
 
+import ch.qos.logback.core.Layout;
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.dom.ElementFactory;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -12,20 +21,98 @@ import com.vaadin.flow.router.Route;
 @Route(value = "Exercise-2", layout = MainLayout.class)
 public class Exercise2View extends VerticalLayout {
 
-    public Exercise2View() {
+    public Exercise2View()
+    {
+        setPadding(false);
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        /*
+        Adds the first name and last name areas.
+         */
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
 
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        FormLayout formLayout = new FormLayout();
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        TextField firstName = new TextField();
+        formLayout.addFormItem(firstName, "First Name");
+        firstName.setWidthFull();
+
+        TextField lastname = new TextField();
+        formLayout.addFormItem(lastname, "Last Name");
+        lastname.setWidthFull();
+
+        horizontalLayout.add(formLayout);
+        horizontalLayout.setSizeFull();
+        horizontalLayout.getStyle().set("background", "yellow");
+        horizontalLayout.expand(formLayout);
+
+        add(horizontalLayout);
+
+        /*
+        Adds the Email area.
+         */
+        HorizontalLayout emailHorizontalLayout = new HorizontalLayout();
+
+        FormLayout emailFormLayout = new FormLayout();
+
+        TextField email = new TextField();
+        emailFormLayout.addFormItem(email, "Email").getElement().setAttribute("colspan", "2");
+        email.setWidthFull();
+
+        emailHorizontalLayout.add(emailFormLayout);
+        emailHorizontalLayout.setSizeFull();
+        emailHorizontalLayout.getStyle().set("background", "pink");
+        emailHorizontalLayout.expand(emailFormLayout);
+
+        add(emailHorizontalLayout);
+
+        /*
+        Adds the Phone area.
+         */
+        HorizontalLayout phoneHorizontalLayout = new HorizontalLayout();
+
+        FormLayout phoneFormLayout = new FormLayout();
+
+        FlexLayout flexPhone = new FlexLayout();
+        flexPhone.setWidthFull();
+        TextField phone = new TextField();
+        Checkbox noCall = new Checkbox("Do Not Call");
+        flexPhone.add(phone, noCall);
+        flexPhone.expand(phone);
+        flexPhone.setAlignItems(Alignment.CENTER);
+
+        phoneFormLayout.addFormItem(flexPhone, "Phone").getElement().setAttribute("colspan", "2");
+
+        phoneHorizontalLayout.add(phoneFormLayout);
+        phoneHorizontalLayout.setSizeFull();
+        phoneHorizontalLayout.getStyle().set("background", "yellow");
+        phoneHorizontalLayout.expand(phoneFormLayout);
+
+        add(phoneHorizontalLayout);
+
+        /*
+        Adds the password and retype password areas.
+         */
+        HorizontalLayout passwordHorizontalLayout = new HorizontalLayout();
+
+        FormLayout passwordFormLayout = new FormLayout();
+
+        PasswordField password = new PasswordField();
+        passwordFormLayout.addFormItem(password, "Password");
+        password.setWidthFull();
+
+        passwordFormLayout.getElement().appendChild(ElementFactory.createBr());
+
+        PasswordField retypePassword = new PasswordField();
+        passwordFormLayout.addFormItem(retypePassword, "Retype-Password");
+        retypePassword.setWidthFull();
+
+        passwordHorizontalLayout.add(passwordFormLayout);
+        passwordHorizontalLayout.setSizeFull();
+        passwordHorizontalLayout.getStyle().set("background", "pink");
+        passwordHorizontalLayout.expand(passwordFormLayout);
+
+        add(passwordHorizontalLayout);
     }
 
 }
